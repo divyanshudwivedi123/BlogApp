b558138a94e53d2cf7043b2356f263260654a102
dd <dwivedidivyanshu30@gmail.com>
2024-06-02 14:03:02 +0530

    Minor improvements done

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -4,13 +4,10 @@ export const Quote = () => {
         <div className="flex justify-center">
             <div className="max-w-lg">
                 <div className="text-3xl font-bold">
-                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
+                    "Serverless Blogging application"
                 </div>
                 <div className="max-w-md text-xl font-semibold text-left mt-4">
-                    Julies Winfield
-                </div>
-                <div className="max-w-md text-sm font-light text-slate-400">
-                    CEO | Acme corp
+                    Made by: Divyanshu Dwivedi
                 </div>
             </div>
         </div>
